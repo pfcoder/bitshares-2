99d17c5616247f0ee9749e04c336c2a32615ff7a
pfcoder <pfcoder97@gmail.com>
2017-01-02 23:07:30 +0800

    Update for CLion project settings

---
+++ mac_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export OPENSSL_ROOT_DIR=/usr/local/opt/openssl/
+export OPENSSL_INCLUDE_DIR=usr/local/opt/openssl/include
+export OPENSSL_LIBRARIES=/usr/local/opt/openssl/lib
+
+export BOOST_INCLUDEDIR=/usr/local/opt/boost159/include/
+export BOOST_LIBRARYDIR=/usr/local/opt/boost159/lib/
+
